9d01f6b302830c5be53b257d63ba543894be11c2
Weslley <weslley@nds.com.br>
2023-05-24 08:00:57 -0400

    Nova credencial

--- public/js/config.js
+++ public/js/config.js
@@ -1,19 +1,19 @@
 turnConfig = {
   iceServers: [
-    {
-      urls: [ "stun:sp-turn1.xirsys.com" ]
-    },
-    {
-      username: "hUWejrv6fgkkx8wMaqdhKZLvVlyvQE1zE9o3CTDiLHa0UuTUpoi36AmUnjtRSyLiAAAAAGRtXUt3ZXNsbGV5dmM=",
-      credential: "c3cb9850-f9cb-11ed-91c3-0242ac120004",
-      urls: [
-        "turn:sp-turn1.xirsys.com:80?transport=udp",
-        "turn:sp-turn1.xirsys.com:3478?transport=udp",
-        "turn:sp-turn1.xirsys.com:80?transport=tcp",
-        "turn:sp-turn1.xirsys.com:3478?transport=tcp",
-        "turns:sp-turn1.xirsys.com:443?transport=tcp",
-        "turns:sp-turn1.xirsys.com:5349?transport=tcp"
-      ]
-    }
-  ]
+	{
+		urls: ["stun:sp-turn1.xirsys.com"]
+	},
+	{
+		username: "s5P6DIjTc-WT8yzdRnu9fGkosqm6Dc7vs4a8q5Eb1vlA7abxIy9ezNzfzB3E6tCYAAAAAGRt-oB3ZXNsbGV5dmM=",
+		credential: "7767ef46-fa29-11ed-9b4d-0242ac120004",
+		urls:[
+			"turn:sp-turn1.xirsys.com:80?transport=udp",
+			"turn:sp-turn1.xirsys.com:3478?transport=udp",
+			"turn:sp-turn1.xirsys.com:80?transport=tcp",
+			"turn:sp-turn1.xirsys.com:3478?transport=tcp",
+			"turns:sp-turn1.xirsys.com:443?transport=tcp",
+			"turns:sp-turn1.xirsys.com:5349?transport=tcp"
+			]
+		}
+	]
 }
