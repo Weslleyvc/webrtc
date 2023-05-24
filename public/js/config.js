2e2e4a5d9ee539a5dc550e0dec6435f202fb4868
Weslley <weslleyvilelacosta@gmail.com>
2023-05-23 22:29:06 -0400

    Primeiro

---
+++ public/js/config.js
@@ -0,0 +1,19 @@
+turnConfig = {
+    iceServers: [
+    {   
+      urls: [ "stun:sp-turn1.xirsys.com" ]
+    }, 
+    {   
+        username: "hUWejrv6fgkkx8wMaqdhKZLvVlyvQE1zE9o3CTDiLHa0UuTUpoi36AmUnjtRSyLiAAAAAGRtXUt3ZXNsbGV5dmM=",
+        credential: "c3cb9850-f9cb-11ed-91c3-0242ac120004",
+      urls: [       
+        "turn:bn-turn1.xirsys.com:80?transport=udp",       
+        "turn:bn-turn1.xirsys.com:3478?transport=udp",       
+        "turn:bn-turn1.xirsys.com:80?transport=tcp",       
+        "turn:bn-turn1.xirsys.com:3478?transport=tcp",       
+        "turns:bn-turn1.xirsys.com:443?transport=tcp",       
+        "turns:bn-turn1.xirsys.com:5349?transport=tcp"   
+       ]
+     }
+   ]
+}
